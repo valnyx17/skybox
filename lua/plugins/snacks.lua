b481629559c1030ca5ee0deed059dd8b8ea8cbf0
valerie <hi@dessa.dev>
2024-11-19 15:25:42 -0500

    add dashboard (snacks.dashboard)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,6 +11,22 @@ require("snacks").setup({
         notification = {
             wo = { wrap = true }
         }
+    },
+    dashboard = {
+        enabled = true,
+        sections = {
+            { section = "header" },
+            {
+                pane = 2,
+                section = "terminal",
+                cmd = "colorscript -e square",
+                height = 5,
+                padding = 1
+            },
+            { section = "keys",  gap = 1,                       padding = 1 },
+            { pane = "2",        icon = tools.ui.icons.file,    title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+            { pane = "2",        icon = tools.ui.icons.diamond, title = "Projects",     section = "projects",     indent = 2, padding = 1 },
+        }
     }
 })
 
