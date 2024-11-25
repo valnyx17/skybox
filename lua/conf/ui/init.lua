81cd68adcad6f91ca22da620d19fbb8c60e82cdf
valerie <hi@dessa.dev>
2024-11-24 23:54:34 -0500

    fix virtual-text appearing when it shouldn't (have plugin for it)

--- lua/conf/ui/init.lua
+++ lua/conf/ui/init.lua
@@ -14,6 +14,7 @@ local lsp_signs = {
 vim.diagnostic.config({
     underline = true,
     severity_sort = true,
+    virtual_text = false,
     float = {
         header = ' ',
         border = tools.ui.cur_border,
