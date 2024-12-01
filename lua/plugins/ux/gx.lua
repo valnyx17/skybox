f3a5f32cec238a996157a56a0d5aa1fb2abf79aa
valerie <hi@dessa.dev>
2024-12-01 11:50:52 -0500

    various ux improvements

---
+++ lua/plugins/ux/gx.lua
@@ -0,0 +1,15 @@
+return {
+    "chrishrb/gx.nvim",
+    keys = {
+        { "gx", "<cmd>Browse<CR>", mode = { "n", "x" }, desc = "open link" }
+    },
+    cmd = { "Browse" },
+    init = function()
+        vim.g.netrw_nogx = 1
+    end,
+    dependencies = {
+        { "nvim-lua/plenary.nvim" }
+    },
+    opts = {},
+    submodules = false,
+}
