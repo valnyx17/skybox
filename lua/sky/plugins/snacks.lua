997dcac49e914b1e224b6242326e05c161b0539a
valerie <hi@dessa.dev>
2025-01-22 21:56:03 -0500

    remove news :)

--- lua/sky/plugins/snacks.lua
+++ lua/sky/plugins/snacks.lua
@@ -200,21 +200,6 @@ return {
             Snacks.scratch()
         end, { silent = true, desc = "Toggle Scratch Buffer" })
 
-        vim.keymap.set("n", "<leader>N", function()
-            Snacks.win({
-                file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-                width = 0.6,
-                height = 0.6,
-                wo = {
-                    spell = false,
-                    wrap = false,
-                    signcolumn = "yes",
-                    statuscolumn = " ",
-                    conceallevel = 3,
-                },
-            })
-        end, { desc = "Neovim News" })
-
         vim.api.nvim_create_autocmd("User", {
             pattern = "MiniFilesActionRename",
             callback = function(event)
